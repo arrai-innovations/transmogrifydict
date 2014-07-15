22068ec2f6c769c1dc4f91bc1f5f9c01ca9bfea3
Joel Hillacre <joel@403forbidden.ca>
2014-07-15 15:07:17 -0600

    bumping version number.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='transmogrifydict',
     url='https://github.com/emergence/transmogrifydict',
-    version='0.0.1',
+    version='0.0.2',
     description='The "turn a dict from one API into a dict for another" python module.',
     author='Emergence by Design',
     author_email='support@emergence.com',
