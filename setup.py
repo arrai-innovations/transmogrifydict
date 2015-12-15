016042716ffcee9009607cc4a55303945c567d14
Joel Hillacre <joel@403forbidden.ca>
2015-12-15 16:26:47 -0700

    bump version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='transmogrifydict',
     url='https://github.com/emergence/transmogrifydict',
-    version='0.0.2',
+    version='1.0.0',
     description='The "turn a dict from one API into a dict for another" python module.',
     author='Emergence by Design',
     author_email='support@emergence.com',
