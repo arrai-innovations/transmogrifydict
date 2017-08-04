9a2416d352472aa87cf297bad0daca9e05a15907
Joel Hillacre <joel@403forbidden.ca>
2017-08-04 16:33:01 -0600

    shutup by default.

--- setup.py
+++ setup.py
@@ -1,9 +1,4 @@
 from setuptools import setup
-import unittest
-
-
-def test_suite():
-    return unittest.TestLoader().discover('tests', pattern='test_*.py')
 
 setup(
     name='transmogrifydict',
@@ -13,6 +8,5 @@ setup(
     author='Emergence by Design',
     author_email='support@emergence.com',
     py_modules=['transmogrifydict'],
-    test_suite='setup.test_suite',
     install_requires=[x for x in open('./requirements.txt').read().split('\n') if x]
 )
