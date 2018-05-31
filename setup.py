f3769732abe1239f8a9647e2bd52aba3b05b1527
Joel Hillacre <joel@403forbidden.ca>
2018-05-31 15:28:24 -0600

    Emergence by Design is changing its name to Arrai Innovations.

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name='transmogrifydict',
-    url='https://github.com/emergence/transmogrifydict',
+    url='https://github.com/arrai-innovations/transmogrifydict',
     version='1.1.1',
     description='The "turn a dict from one API into a dict for another" python module.',
-    author='Emergence by Design',
-    author_email='support@emergence.com',
+    author='Arrai Innovations',
+    author_email='support@arrai.com',
     py_modules=['transmogrifydict'],
     install_requires=[x for x in open('./requirements.txt').read().split('\n') if x]
 )
