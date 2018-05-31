c81677702d80cbbb5703058ee5189c484f4aef4a
Joel Hillacre <joel@403forbidden.ca>
2018-05-31 15:30:46 -0600

    bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='transmogrifydict',
     url='https://github.com/arrai-innovations/transmogrifydict',
-    version='1.1.1',
+    version='1.1.2',
     description='The "turn a dict from one API into a dict for another" python module.',
     author='Arrai Innovations',
     author_email='support@arrai.com',
