8a255d6637b2c5429cd01f9e762b8bc81a8b3b2d
Joel Hillacre <joel@403forbidden.ca>
2019-08-02 17:10:25 -0600

    bump. fix setup.py. closes #12

--- setup.py
+++ setup.py
@@ -2,20 +2,21 @@ from setuptools import setup
 
 with open('README.md', 'r') as fh:
     long_description = fh.read()
-
+with open('./requirements.txt') as reqs:
+    install_requires = [x for x in reqs.read().split('\n') if x]
 setup(
     name='transmogrifydict',
     url='https://github.com/arrai-innovations/transmogrifydict',
-    version='1.1.2',
+    version='1.1.3',
     description='The "turn a dict from one API into a dict for another" python module.',
     long_description=long_description,
-    long_description_conent_type='text/markdown',
+    long_description_content_type='text/markdown',
     author='Arrai Innovations',
     author_email='support@arrai.com',
     py_modules=['transmogrifydict'],
-    install_requires=[x for x in open('./requirements.txt').read().split('\n') if x],
+    install_requires=install_requires,
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
